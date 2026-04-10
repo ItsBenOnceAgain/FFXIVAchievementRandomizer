8bed91d28482f3215d4a4514b309fc25552518d7
Ben Goff <itsbenonceagain@gmail.com>
2026-04-10 12:49:04 -0400

    Frontend design solidified.

--- Frontend/ffxiv-achievement-rando/src/lib/components/AchievementDisplay.svelte
+++ Frontend/ffxiv-achievement-rando/src/lib/components/AchievementDisplay.svelte
@@ -1,28 +1,26 @@
 <script lang="ts">
     import { Achievement } from '$lib/models/models';
-
-    let base_icon_url = "https://v2.xivapi.com/api/asset?format=webp&path="
+    import AchievementIcon from '$lib/components/AchievementIcon.svelte';
+    import AchievementCategory from '$lib/components/AchievementCategory.svelte';
+    import AchievementReward from '$lib/components/AchievementReward.svelte';
+    import '$lib/styles/achievementDisplay.scss';
     let {achievement} : { achievement: Achievement } = $props();
 
 </script>
 
-<section>
-    <h2>Random Achievement</h2>
-    <img src={base_icon_url +achievement.icon_path} alt={achievement.name} width="64" height="64" />
-    <p><strong>Name:</strong> {achievement.name}</p>
-    <p><strong>Description:</strong> {achievement.description}</p>
-    <p><strong>Category:</strong> {achievement.category}</p>
-    {#if achievement.item_reward}
-        <p><strong>Item Reward:</strong> {achievement.item_reward}</p>
-    {/if}
-
-    {#if achievement.title.feminine_title || achievement.title.masculine_title}
-        <p><strong>Title Reward:</strong> 
-            {#if achievement.title.feminine_title === achievement.title.masculine_title}
-                {achievement.title.feminine_title}
-            {:else}
-                {achievement.title.feminine_title} / {achievement.title.masculine_title}
-            {/if}
-        </p>
-    {/if}
+<section class="achievement-body">
+    <div class="achievement-icon">
+        <AchievementIcon icon_path={achievement.icon_path} achievement_name={achievement.name} />
+    </div>
+    <h2 class="achievement-name">{achievement.name}</h2>
+    <h3 class="achievement-description">▶ {achievement.description}</h3>
+    <div class="reward">
+        {#if achievement.item_reward || achievement.title.masculine_title || achievement.title.feminine_title}
+            <AchievementReward achievement={achievement} />
+        {/if}
+    </div>
+    <div class="category">
+        <AchievementCategory category_name={achievement.category} />
+    </div>
+    
 </section>
---
+++ Frontend/ffxiv-achievement-rando/src/lib/components/AchievementIcon.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import '../styles/achievementIcon.scss';
+    import iconWrapper from '../assets/achievementIconWrapper.png';
+    let base_icon_url = "https://v2.xivapi.com/api/asset?format=webp&path="
+    let {icon_path, achievement_name} : { icon_path: string, achievement_name: string } = $props();
+</script>
+
+<div class="icon-wrapper">
+    <img class="overlay base-icon" src={base_icon_url +icon_path} alt={achievement_name}/>
+    <img class="overlay" src={iconWrapper} alt="icon wrapper" width="90" height="90"/>
+</div>
